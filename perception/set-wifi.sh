2c0634cb39cef9ef05d350cc76b6b673b7dd6e2c
joacogomezc <joaquin.gomezcodino@gmail.com>
2023-09-27 23:38:43 +0000

    wifi params and test fix

--- perception/set-wifi.sh
+++ perception/set-wifi.sh
@@ -1,7 +1,9 @@
-SSID=$( grep "^#define WIFI_SSID"  config.h | rev | cut -d'"' -f 2 | rev )
-PASSWORD=$( grep "^#define WIFI_PASSWORD" config.h | rev | cut -d'"' -f 2 | rev )
+SSID=$(grep "^#define WIFI_SSID" /home/iot/Desktop/ceiot_base/perception/esp32c3-bmp280/config.h | rev | cut -d'"' -f 2 | rev)
+PASSWORD=$(grep "^#define WIFI_PASSWORD" /home/iot/Desktop/ceiot_base/perception/esp32c3-bmp280/config.h | rev | cut -d'"' -f 2 | rev)
 
-sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="'${SSID}'"/' 
-sed sdkconfig -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="'${PASSWORD}'"/' 
+sed -i -e 's/CONFIG_EXAMPLE_WIFI_SSID=".*/CONFIG_EXAMPLE_WIFI_SSID="'"${SSID}"'"/' /home/iot/Desktop/ceiot_base/perception/esp32-bmp280/sdkconfig.ci
+sed -i -e 's/CONFIG_EXAMPLE_WIFI_PASSWORD=".*/CONFIG_EXAMPLE_WIFI_PASSWORD="'"${PASSWORD}"'"/' /home/iot/Desktop/ceiot_base/perception/esp32-bmp280/sdkconfig.ci
+
+
 
 
