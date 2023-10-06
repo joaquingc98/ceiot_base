3e5c5ab35e5f676e9e5ee4a2b6da4645150f35a3
joaquingc98 <89083451+joaquingc98@users.noreply.github.com>
2023-10-06 23:53:18 +0000

    Add key to measurments post

--- tools/post_measurement.sh
+++ tools/post_measurement.sh
@@ -1,1 +1,1 @@
-wget -O - --method=POST http://localhost:8080/measurement --body-data="id=$1&t=$2&h=$3"
+wget -O - --method=POST http://localhost:8080/measurement --body-data="id=$1&key=$2&t=$3&h=$4"
